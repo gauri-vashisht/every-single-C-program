5f280570862181758863e9cb0c72a23d3bf52679
gauri-vashisht <poonam.v2002@gmail.com>
2025-01-01 13:05:18 +0530

    Add files via upload

---
+++ Math_Functions.c
@@ -0,0 +1,19 @@
+/*WAP to practice math functions such as sin(), cos(), log(), pow(), sqrt() etc. 
+by including <math.h> header file.*/
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float x;
+    printf("Provide a value to perform functions on: ");
+    scanf("%f", &x);
+    printf("Sin = %f\n", sin(x));
+    printf("Cos = %f\n", cos(x));
+    printf("Log = %f \n", log(x));
+    printf("Square root = %f\n", sqrt(x));
+    float a,b;
+    printf("Provide two values for power function: ");
+    scanf("%f %f",&a,&b);
+    printf("Pow = %f\n", pow(a,b));
+}
