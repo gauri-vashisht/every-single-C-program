52c47fd43b91aff74bf162a18fc6fe7ff30d79a9
gauri-vashisht <poonam.v2002@gmail.com>
2025-02-28 19:56:44 +0530

    Update Leap_Year_alternate.c

--- Leap_Year_alternate.c
+++ Leap_Year_alternate.c
@@ -7,7 +7,7 @@ int main()
     printf("Enter the year: ");
     scanf("%d", &year);
 
-    if ( year % 400 == 0 || year % 100 == 0 && year % 4 == 0 )
+    if ( (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) )
         printf("Leap year");
     else
         printf("Not a leap year");
